d8c0f424b31b0fc33027aa84dc3650f59e62e9ba
Nirvik Purkait <nirvikpurkait@gmail.com>
2024-11-02 10:36:33 +0530

    added sorce code, pre-commit hook, tests

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,31 @@
-console.log("hellooo");
+import colors from "./data/colors.json";
+
+type ColorShade = {
+  [key: string]: string;
+};
+
+type Color = {
+  [key: string]: ColorShade;
+};
+
+const colorDetails: Color = colors;
+
+// warn:
+/**
+ * This function uses O(n^2) complexity. Try to reduce
+ * the complexity
+ */
+export function findColorName(hexColor: string): string | null {
+  const colorNames = Object.keys(colorDetails);
+  for (let colorName of colorNames) {
+    const shades = Object.keys(colorDetails[colorName]);
+
+    for (let shade of shades) {
+      if (hexColor === colorDetails[colorName][shade]) {
+        return `${colorName}-${shade}`;
+      }
+    }
+  }
+
+  return null;
+}
